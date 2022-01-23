3bd95352de2a13b4f7ede7ca3b3b9afd1706208e
LineBreez <alinesn0308@gmail.com>
2022-01-23 18:36:26 -0300

    primeirodesafio

---
+++ script.js
@@ -0,0 +1,17 @@
+let aline
+aline = " aline da silva nogueira";
+const idade = 26;
+
+let number = 26
+console.log ( o valor do numero é $(number))
+
+let year = 2021;
+let birth = 1995;
+​
+​let age = year - birth;
+
+let Estou mexendo no console.log =
+console.log  ("Estou mexendo no console.log")
+
+let quarta feira = true
+console.log ( 0 valor boleano é $(boleano))
